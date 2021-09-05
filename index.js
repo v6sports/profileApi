9669f101b85e585e027481be7d676fcd72a13052
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2021-09-05 12:12:10 +0530

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+app.get('/', (req, res) => {
+   res.status(200).send('hello world!!! ');
+});
+
+app.get('/time', (req, res) => {
+   let timeNow = Date(Date.now());
+   res.status(200).send(timeNow.toString());
+});
+
+module.exports = app; 
