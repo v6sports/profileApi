0bba6fa4393dd1b692a1299b89e105280228484d
Shashwat Singh (alias Ma4akaa1) <shashwatsingh71@gmail.com>
2021-09-11 15:14:51 +0530

    test

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({extended:false}))
-
-app.get('/', (req, res) => {
-   res.status(200).send('HELOO !!! ');
-});
-
-app.get('/time', (req, res) => {
-   let timeNow = Date(Date.now());
-   res.status(200).send(timeNow.toString());
-});
-
-module.exports = app; 
+exports.handler = async (event) => {
+   const response = {
+       statusCode: 200,
+       body: JSON.stringify('Updated Through Github Action 😁'),
+   };
+   return response;
+};
