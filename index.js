b9f87a8ab92f3e5dbfb8f3072f79d3c7519fb015
Mayank Dwivedi <mayankanildwivedi@gmail.com>
2023-07-23 15:28:24 +0530

    Return

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import { Dialect, Sequelize } from 'sequelize'
+// import { Dialect, Sequelize } from 'sequelize'
+const Seq = require('sequelize');
 const dbName = 'usaclive' ||  process.env.DB_NAME
 const dbUser = 'admin' || process.env.DB_USER
 const dbHost = 'v6dbv2.c1gh7vsmbv6p.ap-south-1.rds.amazonaws.com' || process.env.DB_HOST
 const dbDriver = 'mysql'|| process.env.DB_DRIVER 
 const dbPassword =  'vkdbpass1234!' || process.env.DB_PASSWORD
 
-const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
+const sequelizeConnection = new Seq.Sequelize(dbName, dbUser, dbPassword, {
   host: dbHost,
   dialect: dbDriver
 })
