bf2ed9304a79b1d4e6108e63f6a0a169a64ff153
Mayank Dwivedi <mayankanildwivedi@gmail.com>
2023-07-23 15:45:17 +0530

    final q

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
 // import { Dialect, Sequelize } from 'sequelize'
 const Seq = require('sequelize');
-const dbName = 'usaclive' ||  process.env.DB_NAME
+
+const serverless = require('serverless-http');
+const express = require('express')
+const app = express()
+
+const dbName = 'usaclive' || process.env.DB_NAME
 const dbUser = 'admin' || process.env.DB_USER
 const dbHost = 'v6dbv2.c1gh7vsmbv6p.ap-south-1.rds.amazonaws.com' || process.env.DB_HOST
-const dbDriver = 'mysql'|| process.env.DB_DRIVER 
-const dbPassword =  'vkdbpass1234!' || process.env.DB_PASSWORD
+const dbDriver = 'mysql' || process.env.DB_DRIVER
+const dbPassword = 'vkdbpass1234!' || process.env.DB_PASSWORD
 
 const sequelizeConnection = new Seq.Sequelize(dbName, dbUser, dbPassword, {
-  host: dbHost,
-  dialect: dbDriver
+    host: dbHost,
+    dialect: dbDriver
 })
 
-exports.handler = async (event) => {
-    let body = JSON.parse(event.body)
-   try {
-    if(body?.query){
-        sequelizeConnection.query(body?.query).then(response => {
-           return {
-                statusCode: 200,
-                body: 'db',
-            };
-        //    return response[0]
-        }).catch(error=>{
-            return {
+app.post("/", function (req, res) {
+    let body = JSON.parse(req.body)
+    try {
+        if (req?.body?.query) {
+            sequelizeConnection.query(req?.body?.query).then(response => {
+                
+                res.status(200).jsonp(response)
+            }).catch(error => {
+                res.status(200).jsonp( {
+                    statusCode: 200,
+                    body: 'error',
+                });
+            })
+        }
+        else {
+            res.status(200).jsonp( {
                 statusCode: 200,
                 body: 'error',
-            };
-        })
+            });
+        }
     }
-    else {
-        return {
-            statusCode: 200,
-            body:'NO Way',
-        };
+    catch (e) {
+        return e
     }
-   }
-   catch(e) {
-    return e
-   }
-    
-};
+
+});
+
+
+module.exports.handler = serverless(app)
